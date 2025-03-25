013b109d15c6124d5d6fa61637e86e8cab5e9cdf
Barkha Raj <161699296+Mystique5@users.noreply.github.com>
2025-03-26 01:51:10 +0530

    Time: 1 ms (99.99%), Space: 44.3 MB (89.08%) - LeetHub

--- 1482-how-many-numbers-are-smaller-than-the-current-number/1482-how-many-numbers-are-smaller-than-the-current-number.java
+++ 1482-how-many-numbers-are-smaller-than-the-current-number/1482-how-many-numbers-are-smaller-than-the-current-number.java
@@ -1,7 +1,7 @@
 class Solution {
     public int[] smallerNumbersThanCurrent(int[] nums) {
-
-        int result[]=new int[nums.length];
+        //Brute force approach
+       /* int result[]=new int[nums.length];
         Arrays.fill(result, 0);
 
         for(int i=0;i<nums.length;i++)
@@ -14,6 +14,31 @@ class Solution {
                 }
             }
         }
+         return result;*/
+
+         //Optimised O(3N) method 
+
+         int[] count=new int[101];
+         int[] result=new int[nums.length];
+
+         for(int i=0;i<nums.length;i++)
+         {
+            count[nums[i]]++;
+         }
+
+         for(int i=1;i<=100;i++)
+         {
+            count[i]+= count[i-1];
+         }
+
+         for(int i=0;i<nums.length;i++)
+         {
+            if(nums[i]==0)
+                result[i]=0;
+            else
+                result[i]=count[nums[i]-1];
+         }
+
          return result;
         
     }
