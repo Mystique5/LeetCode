7ef9e98e13c3060d52a21e2e10509a0eec0b2d41
Barkha Raj <161699296+Mystique5@users.noreply.github.com>
2025-07-26 13:44:33 +0530

    Time: 30 ms (62.86%), Space: 50.9 MB (74.16%) - LeetHub

--- 1014-k-closest-points-to-origin/1014-k-closest-points-to-origin.java
+++ 1014-k-closest-points-to-origin/1014-k-closest-points-to-origin.java
@@ -1,11 +1,21 @@
 class Solution {
     public int[][] kClosest(int[][] points, int k) {
 
-        Arrays.sort(points, (p1,p2)-> (p1[0]*p1[0] + p1[1]*p1[1]) - (p2[0]*p2[0] + p2[1]*p2[1]));
+        PriorityQueue<int[]> pq = new PriorityQueue<int[]>((p1,p2)-> (p2[0]*p2[0] + p2[1]*p2[1]) - (p1[0]*p1[0] + p1[1]*p1[1]));
+
+        for(int[] p:points)
+        {
+            pq.offer(p);
+            if(pq.size() > k)
+            {
+                pq.poll();
+            }
+        }
+        
         int[][] results=new int[k][2];
         for(int i=0 ; i<k ;i++)
         {
-            results[i]=points[i];
+            results[i]=pq.poll();
         }
 
         return results;
