fb1b1dedbd8670a9d147630edd550d8c9c47c467
Barkha Raj <161699296+Mystique5@users.noreply.github.com>
2025-07-26 16:19:20 +0530

    Time: 0 ms (100%), Space: 46.6 MB (79.43%) - LeetHub

--- 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.java
+++ 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.java
@@ -5,13 +5,12 @@ class Solution {
 
         for(int i=2;i<nums.length;i++)
         {
-            if(nums[i]!=nums[index-2])
+            if(nums[i] !=nums[index-2])
             {
                 nums[index]=nums[i];
                 index++;
             }
         }
         return index;
-        
     }
 }
