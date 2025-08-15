f30934912d1861039eedb358adda4c1648c29303
Barkha Raj <161699296+Mystique5@users.noreply.github.com>
2025-08-15 14:02:41 +0530

    Time: 1 ms (12.62%), Space: 42.4 MB (6.39%) - LeetHub

--- 0101-symmetric-tree/0101-symmetric-tree.java
+++ 0101-symmetric-tree/0101-symmetric-tree.java
@@ -15,13 +15,42 @@
  */
 class Solution {
     public boolean isSymmetric(TreeNode root) {
+    //Approach 1 - recurtion
+    /*   if(root==null)
+            return true;
+        return isMirror(root.left, root.right);*/
+
+    //Approach 2 - iteration
+
         if(root==null)
             return true;
-        return isMirror(root.left, root.right);
+        Queue<TreeNode> left=new LinkedList<>();
+        Queue<TreeNode> right=new LinkedList<>();
+        left.offer(root.left);
+        right.offer(root.right);
+
+        while(!left.isEmpty() && !right.isEmpty())
+        {
+            TreeNode leftNode=left.poll();
+            TreeNode rightNode=right.poll();
+
+            if(leftNode==null && rightNode==null)
+                continue;
+            if(leftNode==null || rightNode==null)
+                return false;
+            if(leftNode.val!=rightNode.val)
+                return false;
+            
+            left.offer(leftNode.left);
+            left.offer(leftNode.right);
+            right.offer(rightNode.right);
+            right.offer(rightNode.left);
+        }
+        return true;
      
     }
 
-    private boolean isMirror(TreeNode leftNode, TreeNode rightNode)
+   /* private boolean isMirror(TreeNode leftNode, TreeNode rightNode)
     {
         if(leftNode ==null && rightNode==null)
             return true;
@@ -30,5 +59,7 @@ class Solution {
         
         return (leftNode.val ==rightNode.val) && isMirror(leftNode.left, rightNode.right) && 
             isMirror(leftNode.right, rightNode.left);
-    }
+    }*/
+
+    
 }
