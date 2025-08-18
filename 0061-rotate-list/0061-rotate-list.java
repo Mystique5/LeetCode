968a322b10e75df26d5eb44754f05850d01ebfa9
Barkha Raj <161699296+Mystique5@users.noreply.github.com>
2025-08-18 18:06:26 +0530

    Time: 0 ms (100%), Space: 43 MB (9.85%) - LeetHub

--- 0061-rotate-list/0061-rotate-list.java
+++ 0061-rotate-list/0061-rotate-list.java
@@ -16,24 +16,27 @@ class Solution {
         ListNode curr=head;
         int count=1;
 
+        //Counting the no of nodes and also in the end curr will point to the last node
         while(curr.next !=null)
         {
             curr=curr.next;
             count++;
         }
 
+        //Checking if k is a multiple of count, if so then after rotation same list will be formed , hence return the same list. 
         if(k%count ==0)
             return head;
-        
+
+        //if not the make the list circular
         curr.next=head;
-        k= k%count;
-        curr=head;
+        k= k%count; // take out the no of times we have to rotate.
+        curr=head; //now point to head and traverse till the new (head-1)th node
         for(int i=0;i<count-k-1;i++)
         {
             curr=curr.next;
         }
-        head=curr.next;
-        curr.next=null;
+        head=curr.next; // make the new head 
+        curr.next=null; // break the circle
 
         return head;
         
